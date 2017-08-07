3bbaf13219e4b1a17cd306a67df46e2e0f337ecc
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-08 00:31:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ function createTemplete (data) {
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-var htmlTemplate =`
+var htmlTemplete = `
 <html>
  <head>
   <title>
