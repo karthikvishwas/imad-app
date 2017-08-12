d617daff8a3f684b352bef3827ac3ada51b6630a
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-12 14:05:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML ='New value';
 
 //move th image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveright (){
+    marginLeft=marginLeft+10;
+    img/style.marginLeft=marginLeft='px';
+}
 img.onclick=function (){
- img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
+ 
 };
