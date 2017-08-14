cff944253f60ff183186267b18df4b19055b2597
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-14 12:48:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,10 @@ button.onclick=function(){
   var request= new XMLHttpRequest();
   //capture the responce and store it in a variable
   request.onreadystatechange=function (){
-      if(request.readystate===XMLHttpRequest.Done){
+      if(request.readystate===XMLHttpRequest.DONE){
           //take actionn
           if(request.status===200){
-            var counter=  request.responceText;
+            var counter =  request.responceText;
              var span=document.getElementById('count');
              span.innerHTML=counter.toString();
           }
@@ -19,6 +19,6 @@ button.onclick=function(){
       // nt done yet
   };
  //make the request
- request.open('GET',"http://http://karthikelc6.imad.hasura-app.io/counter",true);
+ request.open('GET',"http:////karthikelc6.imad.hasura-app.io/counter",true);
  request.send(null);
 };
