bbda682c406628ec9c365f17f0b01f5c12261ba3
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-15 10:08:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,7 @@ submit.onclick=function(){
  //make the request
  var nameInput=document.getElementById('name');
 var name = nameInput.value;
- request.open('GET','http://karthikelc6.imad.hasura-app.io/submit-name?name=' + name,true);
+ request.open('GET',"http://karthikelc6.imad.hasura-app.io/submit-name?name=" + name,true);
  request.send(null);
   //make a request to the server and send the name
   
