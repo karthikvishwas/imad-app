f7ecd169982151180667b6fe0bf007a6aa37929a
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-15 09:06:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick=function(){
 
 //submit name
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
   //make a request to the server and send the name
@@ -34,8 +34,8 @@ submit.onclick=function(){
   var names=['name1', 'name2','name3','name4'];
   var list='';
   for(var i=0;i<names.length;i++){
-      list == '<li>'+names[i]+'</li>';
+      list == '<li>'+ names[i] +'</li>';
   }
  var ul=document.getElementById('namelist');
- ul.innerHTML=list;
+ ul.innerHTML =list;
 };
