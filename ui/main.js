2f72e783625c7d8df039e50daf0838339fe90b07
karthikvishwas <30700467+karthikvishwas@users.noreply.github.com>
2017-08-15 09:54:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,6 @@ button.onclick=function(){
 };
 
 //submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
     //create a request object
@@ -50,6 +48,8 @@ submit.onclick=function(){
       // nt done yet
   };
  //make the request
+ var nameInput=document.getElementById('name');
+var name = nameInput.value;
  request.open('GET',"http://karthikelc6.imad.hasura-app.io/submit-name?name=" +name,true);
  request.send(null);
   //make a request to the server and send the name
